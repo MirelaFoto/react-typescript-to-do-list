7f035eb06eb2b1fae001a368222c88a549799df7
mirela <mirela.foto@hyperting.com>
2022-11-28 12:10:13 +0100

    fix:create todo

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,20 +1,36 @@
 import { Heading } from '@chakra-ui/react'
 import { useState } from 'react'
-import { dummyData, ITodo } from './Todo.types'
+import AddTodo from './AddTodo'
+import { dummyData, ITodo, TodoEnum } from './Todo.types'
 import TodoList from './TodoList'
 
 
 
 const Home = () => {
 const [todoList, setTodoList]= useState(dummyData as ITodo[])
+const [showAddPage, setShowAddPage]= useState(TodoEnum.list)
+const onAddTodo = ()=>{
+    setShowAddPage(TodoEnum.add)
+}
+const showListHandler=()=>{
+    setShowAddPage(TodoEnum.list)
+}
+const addTodoHandler=(data:ITodo)=>{
+    setTodoList([...todoList,data])
 
 
-
+}
 
     return <> 
     <Heading as='h2' textAlign="center" textColor='green'> React-Typescript Todo List </Heading>
-
+   
+    {showAddPage === TodoEnum.list &&  (<> 
+     <input type="button" value="Add todo" onClick={onAddTodo}/>
     <TodoList list={todoList} />
+    </>)}
+    {showAddPage === TodoEnum.add && <AddTodo onBackButton={showListHandler} onSubmitHandler={addTodoHandler} />}
+
+   
     </>
 
 }
--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -10,9 +10,11 @@ list: ITodo[]
   const TodoList = (props: Props) => {
     const {list}= props;
 return <>
-<p>TodoList</p>
+
 {list.map((todo,i) =>
-    <div key={i}>{todo.content}</div>)}
+    <div key={i}>{todo.content} 
+    <input type="button" value="Edit"/>
+    <input type="button" value="Delete"/></div>)}
 </>
   }
   export default TodoList;
