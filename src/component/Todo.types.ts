934cfe911d608ffe1e9ad6caefbaff8eb32ebe89
mirela <mirela.foto@hyperting.com>
2022-11-28 14:29:29 +0100

    fix:save to local storage

--- src/component/Todo.types.ts
+++ src/component/Todo.types.ts
@@ -4,12 +4,6 @@ content:string;
 
 }
 
-export const dummyData : ITodo[] =[{
-id: new Date().toJSON().toString(),
-content: "Dummy data-Create the todo list"
-
-
-}]
 
 export enum TodoEnum{
     list,
